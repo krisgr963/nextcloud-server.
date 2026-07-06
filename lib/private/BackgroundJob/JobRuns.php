nextcloud/server

commit dc5499af4699d6adfbd30f60bc2df26012f8d39d

feat(jobs): clean old job runs
Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

lib/private/BackgroundJob/JobRuns.php
@@ -62,6 +62,18 @@ final readonly class JobRuns implements IJobRuns {
 		return $result === 1;
 	}
 
+	public function deleteBefore(int $timestamp): int {
+		$beforeSnowflake = $this->snowflakeGenerator->minForTimeId($timestamp);
+		$beforeSnowflake = '91480652934574081';
+		$qb = $this->connection->getQueryBuilder();
+		$result = $qb
+			->delete(self::TABLE)
+			->where($qb->expr()->lt('run_id', $qb->createNamedParameter($beforeSnowflake)))
+			->executeStatement();
+
+		return $result;
+	}
+
 	#[Override]
 	public function runningJobs(int $limit = 200): \Generator {
 		$qb = $this->connection->getQueryBuilder();

config/config.sample.php
@@ -3002,4 +3002,12 @@ $CONFIG = [
 	 * Defaults to ``0``.
 	 */
 	'preview_expiration_days' => 0,
+
+	/**
+	 * Delete job runs older than a certain number of days.
+	 * Less than one day is not allowed.
+	 *
+	 * Defaults to ``60``.
+	 */
+	'background_jobs_expiration_days' => 60,
 ];

core/BackgroundJobs/CleanupBackgroundJobsJob.php
@@ -19,12 +19,14 @@ use OCP\IConfig;
 use OCP\IServerInfo;
 use Override;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
 
 class CleanupBackgroundJobsJob extends TimedJob {
 	public function __construct(
 		ITimeFactory $time,
 		private readonly JobRuns $jobRuns,
 		private readonly IServerInfo $serverInfo,
+		private readonly IConfig $config,
 		private readonly LoggerInterface $logger,
 	) {
 		parent::__construct($time);
@@ -35,8 +37,7 @@ class CleanupBackgroundJobsJob extends TimedJob {
 	#[Override]
 	protected function run($argument): void {
 		$this->reapCrashedJobs();
-
-		// TODO Clean oldest jobs
+		$this->cleanOldestRuns();
 	}
 
 	private function reapCrashedJobs(): void {
@@ -65,6 +66,21 @@ class CleanupBackgroundJobsJob extends TimedJob {
 					'runId' => $job->runId,
 				]);
 			}
+		}
+	}
+
+	private function cleanOldestRuns(): void {
+		$daysToKeep = $this->config->getSystemValueInt('background_jobs_expiration_days', 60);
+		if ($daysToKeep < 1) {
+			throw new RuntimeException('Invalid number of days');
+		}
+		$cleanBeforeTimestamp = time() - ($daysToKeep * 24 * 3600);
+
+		$cleanedJobs = $this->jobRuns->deleteBefore($cleanBeforeTimestamp);
+		if ($cleanedJobs > 0) {
+			$this->logger->info(
+				'Cleanup of old background jobs. Number of jobs removed: ' . $cleanedJobs . 'Reason: older than ' . $daysToKeep . ' days.',
+			);
 		}
 	}
 }

krisgr963/nextcloud-server.

commit 53c47308bb608a763f9effaba506413ae6684bd2

fix(jobs): restore background_jobs_expiration_days behavior
Snowflake ID in `JobRuns:deleteBefore()` is computed from given
timestamp, but immediately overwritten by a literal value making the
expiration setting have no effect.

This looks like a leftover artifact from development that should be
cleaned up. Remove the hardcoded ID to restore the desired behavior.

Fixes: dc5499af4699d6adfbd30f60bc2df26012f8d39d
Suggested-by: Michele Marcionelli
Signed-off-by: Stefan Kalscheuer <stefan@stklcode.de>

lib/private/BackgroundJob/JobRuns.php
@@ -64,7 +64,6 @@ final readonly class JobRuns implements IJobRuns {
 
 	public function deleteBefore(int $timestamp): int {
 		$beforeSnowflake = $this->snowflakeGenerator->minForTimeId($timestamp);
-		$beforeSnowflake = '91480652934574081';
 		$qb = $this->connection->getQueryBuilder();
 		$result = $qb
 			->delete(self::TABLE)
nextcloud/server

commit 60ce92a697dd357465db65f11e10c85cc5404d70

feat(utils): add getter for serverid with proper default
Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

core/BackgroundJobs/CleanupBackgroundJobsJob.php
@@ -1,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\Core\BackgroundJobs;
+
+use DateTimeImmutable;
+use OC\BackgroundJob\JobRuns;
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\BackgroundJob\IJob;
+use OCP\BackgroundJob\JobStatus;
+use OCP\BackgroundJob\TimedJob;
+use OCP\IConfig;
+use OCP\IServerInfo;
+use Override;
+
+class CleanupBackgroundJobsJob extends TimedJob {
+	public function __construct(
+		ITimeFactory $time,
+		private readonly JobRuns $jobRuns,
+		private readonly IServerInfo $serverInfo,
+		private readonly LoggerInterface $logger,
+	) {
+		parent::__construct($time);
+		$this->setInterval(60 * 60);
+		$this->setTimeSensitivity(IJob::TIME_INSENSITIVE);
+	}
+
+	#[Override]
+	protected function run($argument): void {
+		$this->reapCrashedJobs();
+
+		// TODO Clean oldest jobs
+	}
+
+	private function reapCrashedJobs(): void {
+		$currentServerId = $this->serverInfo->getServerId();
+
+		foreach ($this->jobRuns->runningJobs(1000) as $job) {
+			if ($job->serverId !== $currentServerId) {
+				continue;
+			}
+			exec('ps -p ' . escapeshellarg((string)$job->pid) . ' -o cmd', $output, $result);
+			if (count($output) === 1 && $output[0] === 'CMD' && $result === 1) {
+				// Process doesn't exists anymore
+				$maxDuration = (new DateTimeImmutable())->diff($job->startedAt);
+				$maxDuration =
+					($maxDuration->format('%a') * 24 * 60 * 60 * 1000)
+					+ ($maxDuration->format('%h') * 60 * 60 * 1000)
+					+ ($maxDuration->format('%m') * 60 * 1000)
+					+ ($maxDuration->format('%s') * 1000)
+					+ (int)($maxDuration->format('%f') / 1000);
+				$this->jobRuns->finished($job->runId, $maxDuration, 0, JobStatus::CRASHED);
+			}
+		}
+	}
+}

core/Command/Background/JobsHistory.php
@@ -12,7 +12,7 @@ namespace OC\Core\Command\Background;
 use OC\BackgroundJob\JobRuns;
 use OC\Core\Command\Base;
 use OCP\BackgroundJob\JobStatus;
-use OCP\IConfig;
+use OCP\IServerInfo;
 use OCP\Util;
 use Override;
 use Symfony\Component\Console\Input\InputInterface;
@@ -23,7 +23,7 @@ use ValueError;
 final class JobsHistory extends Base {
 	public function __construct(
 		private readonly JobRuns $jobRuns,
-		private IConfig $config,
+		private readonly IServerInfo $serverInfo,
 	) {
 		parent::__construct();
 	}
@@ -75,7 +75,7 @@ final class JobsHistory extends Base {
 	private function formatLine(iterable $jobs): \Generator {
 		$jobsInfo = [];
 		$now = time();
-		$currentServerId = $this->config->getSystemValueInt('serverid', -1);
+		$currentServerId = $this->serverInfo->getServerId();
 		foreach ($jobs as $job) {
 			$status = match ($job->status) {
 				JobStatus::RUNNING => 'Running',

core/Command/Background/RunningJobs.php
@@ -11,7 +11,7 @@ namespace OC\Core\Command\Background;
 
 use OC\BackgroundJob\JobRuns;
 use OC\Core\Command\Base;
-use OCP\IConfig;
+use OCP\IServerInfo;
 use Override;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -20,7 +20,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class RunningJobs extends Base {
 	public function __construct(
 		private readonly JobRuns $jobRuns,
-		private IConfig $config,
+		private readonly IServerInfo $serverInfo,
 	) {
 		parent::__construct();
 	}
@@ -60,7 +60,7 @@ final class RunningJobs extends Base {
 
 	private function formatLine(iterable $jobs): \Generator {
 		$now = time();
-		$currentServerId = $this->config->getSystemValueInt('serverid', -1);
+		$currentServerId = $this->serverInfo->getServerId();
 		foreach ($jobs as $job) {
 			yield [
 				'Run ID' => $job->runId,

lib/composer/composer/autoload_classmap.php
@@ -651,6 +651,7 @@ return array(
     'OCP\\IRequest' => $baseDir . '/lib/public/IRequest.php',
     'OCP\\IRequestId' => $baseDir . '/lib/public/IRequestId.php',
     'OCP\\IServerContainer' => $baseDir . '/lib/public/IServerContainer.php',
+    'OCP\\IServerInfo' => $baseDir . '/lib/public/IServerInfo.php',
     'OCP\\ISession' => $baseDir . '/lib/public/ISession.php',
     'OCP\\IStreamImage' => $baseDir . '/lib/public/IStreamImage.php',
     'OCP\\ITagManager' => $baseDir . '/lib/public/ITagManager.php',
@@ -2173,6 +2174,7 @@ return array(
     'OC\\Security\\VerificationToken\\VerificationToken' => $baseDir . '/lib/private/Security/VerificationToken/VerificationToken.php',
     'OC\\Server' => $baseDir . '/lib/private/Server.php',
     'OC\\ServerContainer' => $baseDir . '/lib/private/ServerContainer.php',
+    'OC\\ServerInfo' => $baseDir . '/lib/private/ServerInfo.php',
     'OC\\ServerNotAvailableException' => $baseDir . '/lib/private/ServerNotAvailableException.php',
     'OC\\ServiceUnavailableException' => $baseDir . '/lib/private/ServiceUnavailableException.php',
     'OC\\Session\\CryptoSessionData' => $baseDir . '/lib/private/Session/CryptoSessionData.php',

lib/composer/composer/autoload_static.php
@@ -692,6 +692,7 @@ class ComposerStaticInit749170dad3f5e7f9ca158f5a9f04f6a2
         'OCP\\IRequest' => __DIR__ . '/../../..' . '/lib/public/IRequest.php',
         'OCP\\IRequestId' => __DIR__ . '/../../..' . '/lib/public/IRequestId.php',
         'OCP\\IServerContainer' => __DIR__ . '/../../..' . '/lib/public/IServerContainer.php',
+        'OCP\\IServerInfo' => __DIR__ . '/../../..' . '/lib/public/IServerInfo.php',
         'OCP\\ISession' => __DIR__ . '/../../..' . '/lib/public/ISession.php',
         'OCP\\IStreamImage' => __DIR__ . '/../../..' . '/lib/public/IStreamImage.php',
         'OCP\\ITagManager' => __DIR__ . '/../../..' . '/lib/public/ITagManager.php',
@@ -2214,6 +2215,7 @@ class ComposerStaticInit749170dad3f5e7f9ca158f5a9f04f6a2
         'OC\\Security\\VerificationToken\\VerificationToken' => __DIR__ . '/../../..' . '/lib/private/Security/VerificationToken/VerificationToken.php',
         'OC\\Server' => __DIR__ . '/../../..' . '/lib/private/Server.php',
         'OC\\ServerContainer' => __DIR__ . '/../../..' . '/lib/private/ServerContainer.php',
+        'OC\\ServerInfo' => __DIR__ . '/../../..' . '/lib/private/ServerInfo.php',
         'OC\\ServerNotAvailableException' => __DIR__ . '/../../..' . '/lib/private/ServerNotAvailableException.php',
         'OC\\ServiceUnavailableException' => __DIR__ . '/../../..' . '/lib/private/ServiceUnavailableException.php',
         'OC\\Session\\CryptoSessionData' => __DIR__ . '/../../..' . '/lib/private/Session/CryptoSessionData.php',

lib/private/Repair/AddCleanupBackgroundJobsJob.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC\Repair;
+
+use OC\Core\BackgroundJobs\CleanupBackgroundJobsJob;
+use OCP\BackgroundJob\IJobList;
+use OCP\Migration\IOutput;
+use OCP\Migration\IRepairStep;
+use Override;
+
+class AddCleanupBackgroundJobsJob implements IRepairStep {
+	public function __construct(
+		private readonly IJobList $jobList,
+	) {
+	}
+
+	#[\Override]
+	public function getName(): string {
+		return 'Cleanup completed background jobs';
+	}
+
+	#[Override]
+	public function run(IOutput $output): void {
+		$this->jobList->add(CleanupBackgroundJobsJob::class);
+	}
+}

lib/private/Server.php
@@ -227,6 +227,7 @@ use OCP\IPreview;
 use OCP\IRequest;
 use OCP\IRequestId;
 use OCP\IServerContainer;
+use OCP\IServerInfo;
 use OCP\ISession;
 use OCP\ITagManager;
 use OCP\ITempManager;
@@ -1146,6 +1147,9 @@ class Server extends ServerContainer implements IServerContainer {
 
 			return $c->get(FileSequence::class);
 		}, false);
+		$this->registerAlias(ISnowflakeDecoder::class, SnowflakeDecoder::class);
+		$this->registerAlias(IJobRuns::class, JobRuns::class);
+		$this->registerAlias(IServerInfo::class, ServerInfo::class);
 
 		$this->connectDispatcher();
 	}

lib/private/ServerInfo.php
@@ -1,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OC;
+
+use OCP\IConfig;
+use OCP\IServerInfo;
+use Override;
+
+readonly class ServerInfo implements IServerInfo {
+	public function __construct(
+		private IConfig $config,
+	) {
+	}
+
+	#[Override]
+	public function getServerId(): int {
+		$serverid = $this->config->getSystemValueInt('serverid', -1);
+		if ($serverid < 1) {
+			// Fallback: generates a server ID based on hostname
+			/** @var int<0,max> */
+			$serverid = PHP_INT_SIZE === 4
+				? hexdec(hash('xxh32', $this->getHostname()))
+				// Makes sure it doesn't overflow 32 bits int
+				: hexdec(substr(hash('xxh32', $this->getHostname()), -3));
+		}
+
+		/** @var int<0,511> */
+		return $serverid & 0x1FF;
+	}
+
+	#[Override]
+	public function getHostname(): string {
+		$hostname = gethostname();
+		if ($hostname === false) {
+			// Use a random hostname
+			return bin2hex(random_bytes(8));
+		}
+
+		return $hostname;
+	}
+}

krisgr963/nextcloud-server.

commit 828995f79edfd93f8181083a27200e4e1f3b75c0

fix(jobs): resolve portability issues with background job cleanup
The previous implementation executes "ps" with flags -p and -o to check
whether a job process was still running. This approach has portability
issues regarding e.g. BusyBox ps (Alpine Linux) where `ps -p` fails or
BSD/macOS where `ps -o` fails.

Replace with `posix_kill($pid, 0)`, the standard POSIX way to probe
process existence without delivering a signal. EPERM is treated as
"process exists but not owned by current user". This should be available
on all supported platforms.

Fixes: 60ce92a697dd357465db65f11e10c85cc5404d70
Suggested-by: Michele Marcionelli
Signed-off-by: Stefan Kalscheuer <stefan@stklcode.de>

core/BackgroundJobs/CleanupBackgroundJobsJob.php
@@ -47,11 +47,9 @@ class CleanupBackgroundJobsJob extends TimedJob {
 			if ($job->serverId !== $currentServerId) {
 				continue;
 			}
-			$output = [];
-			$result = 0;
-			exec('ps -p ' . escapeshellarg((string)$job->pid) . ' -o cmd', $output, $result);
-			if (count($output) === 1 && current($output) === 'CMD' && $result === 1) {
-				// Process doesn't exists anymore
+			$processExists = posix_kill($job->pid, 0) || posix_get_last_error() === 1 /* EPERM */;
+			if (!$processExists) {
+				// Process doesn't exist anymore
 				$maxDuration = (new DateTimeImmutable())->diff($job->startedAt);
 				$maxDuration
 					= ($maxDuration->days * 24 * 60 * 60 * 1000)
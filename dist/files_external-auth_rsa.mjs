bygadd/server

fix(files_external): prevent dropdown heights from shrinking
Signed-off-by: Kent Delante <kent@delante.me>

dist/files_external-auth_rsa.mjs
@@ -1,3 +1,3 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('', '', 'dist/AuthMechanismRsa-Bue2F6rp.chunk.mjs'),window.OC.filePath('', '', 'dist/Web-BwmPK40i.chunk.mjs'),window.OC.filePath('', '', 'dist/public-C1mLBHT3.chunk.mjs'),window.OC.filePath('', '', 'dist/index-BLNGy7h5.chunk.mjs'),window.OC.filePath('', '', 'dist/index-B-dGqfIG.chunk.mjs'),window.OC.filePath('', '', 'dist/util-Alk1iwuj.chunk.mjs'),window.OC.filePath('', '', 'dist/index-DL1yHC1K-DnBG6QLJ.chunk.mjs'),window.OC.filePath('', '', 'dist/index-BW4M2n71.chunk.mjs'),window.OC.filePath('', '', 'dist/NcModal-DUWLRm_F-CU-AeBE5.chunk.mjs'),window.OC.filePath('', '', 'dist/logger-D3RVzcfQ-8mOgKmZ4.chunk.mjs'),window.OC.filePath('', '', 'dist/createElementId-DhjFt1I9-B4kXTdvj.chunk.mjs'),window.OC.filePath('', '', 'dist/translation-DoG5ZELJ-CFYnqluG.chunk.mjs'),window.OC.filePath('', '', 'dist/common-createElementId-DhjFt1I9-C_oBIsvc.chunk.css'),window.OC.filePath('', '', 'dist/common-logger-D3RVzcfQ-BE3lcFW4.chunk.css'),window.OC.filePath('', '', 'dist/common-NcModal-DUWLRm_F-BsKc2RT9.chunk.css'),window.OC.filePath('', '', 'dist/TrashCanOutline-Cv7t-yKN.chunk.mjs'),window.OC.filePath('', '', 'dist/common-TrashCanOutline-BYHcrfvW.chunk.css'),window.OC.filePath('', '', 'dist/common-index-DYA_tnKg.chunk.css'),window.OC.filePath('', '', 'dist/mdi-Lt-19ASw.chunk.mjs'),window.OC.filePath('', '', 'dist/common-mdi-BWNFKLbC.chunk.css'),window.OC.filePath('', '', 'dist/index-BOH7nBEM.chunk.mjs'),window.OC.filePath('', '', 'dist/common-index-980JFXIN.chunk.css'),window.OC.filePath('', '', 'dist/NcCheckboxRadioSwitch-D8Dfv4iw-ChIm2mK6.chunk.mjs'),window.OC.filePath('', '', 'dist/common-NcCheckboxRadioSwitch-D8Dfv4iw-CPGkDj-p.chunk.css'),window.OC.filePath('', '', 'dist/NcPasswordField-Be5_qcfm-ZLEgTVOQ.chunk.mjs'),window.OC.filePath('', '', 'dist/NcInputField-B1bGxYHt-BybY_CkS.chunk.mjs'),window.OC.filePath('', '', 'dist/common-NcInputField-B1bGxYHt-BYHxARP-.chunk.css'),window.OC.filePath('', '', 'dist/common-NcPasswordField-Be5_qcfm-Crp_EMfV.chunk.css'),window.OC.filePath('', '', 'dist/NcTextField.vue_vue_type_script_setup_true_lang-BQHjkK8r-BU5OWoch.chunk.mjs'),window.OC.filePath('', '', 'dist/types-BINwlUX8.chunk.mjs'),window.OC.filePath('', '', 'dist/logger-n22jyIXx.chunk.mjs')])))=>i.map(i=>d[i]);
-import{d as t,a as i,_ as a}from"./Web-BwmPK40i.chunk.mjs";import"./public-C1mLBHT3.chunk.mjs";const r=i(()=>a(()=>import("./AuthMechanismRsa-Bue2F6rp.chunk.mjs").then(e=>e.A),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]),import.meta.url)),s=t(r,{shadowRoot:!1});customElements.define("files_external-auth-mechanism-rsa",s),window.OCA.FilesExternal.AuthMechanism.registerHandler({id:"rsa",tagName:"files_external-auth-mechanism-rsa",enabled(e){return e.scheme==="publickey"&&e.identifier==="publickey::rsa"}});
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('', '', 'dist/AuthMechanismRsa-DG_XgyTI.chunk.mjs'),window.OC.filePath('', '', 'dist/Web-BwmPK40i.chunk.mjs'),window.OC.filePath('', '', 'dist/public-C1mLBHT3.chunk.mjs'),window.OC.filePath('', '', 'dist/index-BLNGy7h5.chunk.mjs'),window.OC.filePath('', '', 'dist/index-B-dGqfIG.chunk.mjs'),window.OC.filePath('', '', 'dist/util-Alk1iwuj.chunk.mjs'),window.OC.filePath('', '', 'dist/index-DL1yHC1K-DnBG6QLJ.chunk.mjs'),window.OC.filePath('', '', 'dist/index-BW4M2n71.chunk.mjs'),window.OC.filePath('', '', 'dist/NcModal-DUWLRm_F-CU-AeBE5.chunk.mjs'),window.OC.filePath('', '', 'dist/logger-D3RVzcfQ-8mOgKmZ4.chunk.mjs'),window.OC.filePath('', '', 'dist/createElementId-DhjFt1I9-B4kXTdvj.chunk.mjs'),window.OC.filePath('', '', 'dist/translation-DoG5ZELJ-CFYnqluG.chunk.mjs'),window.OC.filePath('', '', 'dist/common-createElementId-DhjFt1I9-C_oBIsvc.chunk.css'),window.OC.filePath('', '', 'dist/common-logger-D3RVzcfQ-BE3lcFW4.chunk.css'),window.OC.filePath('', '', 'dist/common-NcModal-DUWLRm_F-BsKc2RT9.chunk.css'),window.OC.filePath('', '', 'dist/TrashCanOutline-Cv7t-yKN.chunk.mjs'),window.OC.filePath('', '', 'dist/common-TrashCanOutline-BYHcrfvW.chunk.css'),window.OC.filePath('', '', 'dist/common-index-DYA_tnKg.chunk.css'),window.OC.filePath('', '', 'dist/mdi-Lt-19ASw.chunk.mjs'),window.OC.filePath('', '', 'dist/common-mdi-BWNFKLbC.chunk.css'),window.OC.filePath('', '', 'dist/index-BOH7nBEM.chunk.mjs'),window.OC.filePath('', '', 'dist/common-index-980JFXIN.chunk.css'),window.OC.filePath('', '', 'dist/NcCheckboxRadioSwitch-D8Dfv4iw-ChIm2mK6.chunk.mjs'),window.OC.filePath('', '', 'dist/common-NcCheckboxRadioSwitch-D8Dfv4iw-CPGkDj-p.chunk.css'),window.OC.filePath('', '', 'dist/NcPasswordField-Be5_qcfm-ZLEgTVOQ.chunk.mjs'),window.OC.filePath('', '', 'dist/NcInputField-B1bGxYHt-BybY_CkS.chunk.mjs'),window.OC.filePath('', '', 'dist/common-NcInputField-B1bGxYHt-BYHxARP-.chunk.css'),window.OC.filePath('', '', 'dist/common-NcPasswordField-Be5_qcfm-Crp_EMfV.chunk.css'),window.OC.filePath('', '', 'dist/NcTextField.vue_vue_type_script_setup_true_lang-BQHjkK8r-BU5OWoch.chunk.mjs'),window.OC.filePath('', '', 'dist/types-BINwlUX8.chunk.mjs'),window.OC.filePath('', '', 'dist/logger-n22jyIXx.chunk.mjs'),window.OC.filePath('', '', 'dist/common-AuthMechanismRsa-DGFpo-hc.chunk.css')])))=>i.map(i=>d[i]);
+import{d as t,a as i,_ as a}from"./Web-BwmPK40i.chunk.mjs";import"./public-C1mLBHT3.chunk.mjs";const r=i(()=>a(()=>import("./AuthMechanismRsa-DG_XgyTI.chunk.mjs").then(e=>e.A),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]),import.meta.url)),s=t(r,{shadowRoot:!1});customElements.define("files_external-auth-mechanism-rsa",s),window.OCA.FilesExternal.AuthMechanism.registerHandler({id:"rsa",tagName:"files_external-auth-mechanism-rsa",enabled(e){return e.scheme==="publickey"&&e.identifier==="publickey::rsa"}});
 //# sourceMappingURL=files_external-auth_rsa.mjs.map

krisgr963/nextcloud-server.

dist/files_external-auth_rsa.mjs
@@ -1,3 +1,3 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('', '', 'dist/AuthMechanismRsa-u97_lnT4.chunk.mjs'),window.OC.filePath('', '', 'dist/Web-DipEvAM1.chunk.mjs'),window.OC.filePath('', '', 'dist/public-C1mLBHT3.chunk.mjs'),window.OC.filePath('', '', 'dist/index-BFQdpkfu.chunk.mjs'),window.OC.filePath('', '', 'dist/index-Cp0kcsps.chunk.mjs'),window.OC.filePath('', '', 'dist/util-DBjHxD9z.chunk.mjs'),window.OC.filePath('', '', 'dist/index-DL1yHC1K-BgjILgEs.chunk.mjs'),window.OC.filePath('', '', 'dist/index-Bba-EfET.chunk.mjs'),window.OC.filePath('', '', 'dist/NcModal-B-YLND3e-CkZ6kVHj.chunk.mjs'),window.OC.filePath('', '', 'dist/logger-D3RVzcfQ-DGD7l52c.chunk.mjs'),window.OC.filePath('', '', 'dist/createElementId-DhjFt1I9-Et9tKIxm.chunk.mjs'),window.OC.filePath('', '', 'dist/translation-DoG5ZELJ-Cix2nzZD.chunk.mjs'),window.OC.filePath('', '', 'dist/common-createElementId-DhjFt1I9-C_oBIsvc.chunk.css'),window.OC.filePath('', '', 'dist/common-logger-D3RVzcfQ-BE3lcFW4.chunk.css'),window.OC.filePath('', '', 'dist/common-NcModal-B-YLND3e-CirRoP1Y.chunk.css'),window.OC.filePath('', '', 'dist/TrashCanOutline-qUYPNZuX.chunk.mjs'),window.OC.filePath('', '', 'dist/common-TrashCanOutline-BYHcrfvW.chunk.css'),window.OC.filePath('', '', 'dist/common-index-DYA_tnKg.chunk.css'),window.OC.filePath('', '', 'dist/mdi-C1U8q2ba.chunk.mjs'),window.OC.filePath('', '', 'dist/common-mdi-BWNFKLbC.chunk.css'),window.OC.filePath('', '', 'dist/index-ZIXfkejI.chunk.mjs'),window.OC.filePath('', '', 'dist/common-index-BWhNCeJl.chunk.css'),window.OC.filePath('', '', 'dist/NcCheckboxRadioSwitch-BVTMQSAg-DacvAHW1.chunk.mjs'),window.OC.filePath('', '', 'dist/common-NcCheckboxRadioSwitch-BVTMQSAg-Ck9WjWwQ.chunk.css'),window.OC.filePath('', '', 'dist/NcPasswordField-Be5_qcfm-CrRxoSpQ.chunk.mjs'),window.OC.filePath('', '', 'dist/NcInputField-B1bGxYHt-DRAiZwqK.chunk.mjs'),window.OC.filePath('', '', 'dist/common-NcInputField-B1bGxYHt-BYHxARP-.chunk.css'),window.OC.filePath('', '', 'dist/common-NcPasswordField-Be5_qcfm-Crp_EMfV.chunk.css'),window.OC.filePath('', '', 'dist/NcTextField.vue_vue_type_script_setup_true_lang-BQHjkK8r-B7pCt4h9.chunk.mjs'),window.OC.filePath('', '', 'dist/types-Ccm1pe7Y.chunk.mjs'),window.OC.filePath('', '', 'dist/logger-BFB83ufD.chunk.mjs')])))=>i.map(i=>d[i]);
-import{d as t,a as i,_ as a}from"./Web-DipEvAM1.chunk.mjs";import"./public-C1mLBHT3.chunk.mjs";const r=i(()=>a(()=>import("./AuthMechanismRsa-u97_lnT4.chunk.mjs").then(e=>e.A),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]),import.meta.url)),s=t(r,{shadowRoot:!1});customElements.define("files_external-auth-mechanism-rsa",s),window.OCA.FilesExternal.AuthMechanism.registerHandler({id:"rsa",tagName:"files_external-auth-mechanism-rsa",enabled(e){return e.scheme==="publickey"&&e.identifier==="publickey::rsa"}});
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('', '', 'dist/AuthMechanismRsa-CqABlUgk.chunk.mjs'),window.OC.filePath('', '', 'dist/Web-DipEvAM1.chunk.mjs'),window.OC.filePath('', '', 'dist/public-C1mLBHT3.chunk.mjs'),window.OC.filePath('', '', 'dist/index-BFQdpkfu.chunk.mjs'),window.OC.filePath('', '', 'dist/index-Cp0kcsps.chunk.mjs'),window.OC.filePath('', '', 'dist/util-DBjHxD9z.chunk.mjs'),window.OC.filePath('', '', 'dist/index-DL1yHC1K-BgjILgEs.chunk.mjs'),window.OC.filePath('', '', 'dist/index-Bba-EfET.chunk.mjs'),window.OC.filePath('', '', 'dist/NcModal-B-YLND3e-CkZ6kVHj.chunk.mjs'),window.OC.filePath('', '', 'dist/logger-D3RVzcfQ-DGD7l52c.chunk.mjs'),window.OC.filePath('', '', 'dist/createElementId-DhjFt1I9-Et9tKIxm.chunk.mjs'),window.OC.filePath('', '', 'dist/translation-DoG5ZELJ-Cix2nzZD.chunk.mjs'),window.OC.filePath('', '', 'dist/common-createElementId-DhjFt1I9-C_oBIsvc.chunk.css'),window.OC.filePath('', '', 'dist/common-logger-D3RVzcfQ-BE3lcFW4.chunk.css'),window.OC.filePath('', '', 'dist/common-NcModal-B-YLND3e-CirRoP1Y.chunk.css'),window.OC.filePath('', '', 'dist/TrashCanOutline-qUYPNZuX.chunk.mjs'),window.OC.filePath('', '', 'dist/common-TrashCanOutline-BYHcrfvW.chunk.css'),window.OC.filePath('', '', 'dist/common-index-DYA_tnKg.chunk.css'),window.OC.filePath('', '', 'dist/mdi-C1U8q2ba.chunk.mjs'),window.OC.filePath('', '', 'dist/common-mdi-BWNFKLbC.chunk.css'),window.OC.filePath('', '', 'dist/index-ZIXfkejI.chunk.mjs'),window.OC.filePath('', '', 'dist/common-index-BWhNCeJl.chunk.css'),window.OC.filePath('', '', 'dist/NcCheckboxRadioSwitch-BVTMQSAg-DacvAHW1.chunk.mjs'),window.OC.filePath('', '', 'dist/common-NcCheckboxRadioSwitch-BVTMQSAg-Ck9WjWwQ.chunk.css'),window.OC.filePath('', '', 'dist/NcPasswordField-Be5_qcfm-CrRxoSpQ.chunk.mjs'),window.OC.filePath('', '', 'dist/NcInputField-B1bGxYHt-DRAiZwqK.chunk.mjs'),window.OC.filePath('', '', 'dist/common-NcInputField-B1bGxYHt-BYHxARP-.chunk.css'),window.OC.filePath('', '', 'dist/common-NcPasswordField-Be5_qcfm-Crp_EMfV.chunk.css'),window.OC.filePath('', '', 'dist/NcTextField.vue_vue_type_script_setup_true_lang-BQHjkK8r-B7pCt4h9.chunk.mjs'),window.OC.filePath('', '', 'dist/types-Ccm1pe7Y.chunk.mjs'),window.OC.filePath('', '', 'dist/logger-BFB83ufD.chunk.mjs'),window.OC.filePath('', '', 'dist/common-AuthMechanismRsa-DGFpo-hc.chunk.css')])))=>i.map(i=>d[i]);
+import{d as t,a as i,_ as a}from"./Web-DipEvAM1.chunk.mjs";import"./public-C1mLBHT3.chunk.mjs";const r=i(()=>a(()=>import("./AuthMechanismRsa-CqABlUgk.chunk.mjs").then(e=>e.A),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]),import.meta.url)),s=t(r,{shadowRoot:!1});customElements.define("files_external-auth-mechanism-rsa",s),window.OCA.FilesExternal.AuthMechanism.registerHandler({id:"rsa",tagName:"files_external-auth-mechanism-rsa",enabled(e){return e.scheme==="publickey"&&e.identifier==="publickey::rsa"}});
 //# sourceMappingURL=files_external-auth_rsa.mjs.map